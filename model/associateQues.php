15852519e63893957ed06afcbbb6f82e7e4457a6
Ankit Gupta <ankigupt@gmail.com>
2016-08-15 22:20:48 +0530

    fixed wrongly overwritten model/associateQues.php

--- model/associateQues.php
+++ model/associateQues.php
@@ -1,55 +1,15 @@
-<html>
-<head>
-<script language="javascript">
-function checkAll(formname, checktoggle)
+<?php
+require_once('../model/db.php');
+function associateQues($quizid,$quesid)
 {
-  var checkboxes = new Array(); 
-  checkboxes = document[formname].getElementsByTagName('input');
- 
-  for (var i=0; i<checkboxes.length; i++)  {
-    if (checkboxes[i].type == 'checkbox')   {
-      checkboxes[i].checked = checktoggle;
-    }
-  }
+	$database_obj=connectDB();
+	$sql="insert into quizquestion values ('".mysql_real_escape_string($quizid)."','".mysql_real_escape_string($quesid)."');";
+	$database_obj->query($sql);
 }
-</script>
-
-<title>
-Select Questions
-</title>
-</head>
-<body>
-<form action='../controller/associateQues.php' name='secques' method='post'>
-<a href="javascript: checkAll('secques','checked');" >Check All</a>
-<a href="javascript: checkAll('secques','');" >Uncheck All</a>
-<input type='hidden' name='queid' value='<?php echo $_REQUEST['id']; ?>' />
-<?php
-    require_once('../model/db.php');
-    $database_obj=connectDB();
-    $sql  = 'select * from category';
-    foreach($database_obj->query($sql) as $row)
-    {
+function deleteQues($quizid)
+{
+	$database_obj=connectDB();
+	$sql="delete from quizquestion where QuizId=".$quizid;
+	$database_obj->query($sql);
+}
 ?>
-	<div> <b> <?php echo $row['CatName']; ?> </b></div>
-<?php
-	$sql='select * from questionbase where CatId='.$row['CatId'];
-	foreach($database_obj->query($sql) as $quesrow)
-	{
-?>
-<input type='checkbox' name='queslist[]' value="<?php echo $quesrow['QueId']; ?>" /> <?php echo $quesrow['Question']; ?> 
-	<br/>
-	<?php if($quesrow['imageid'] != 0) {
-	?>
-		<img src="../view/upload/showimg.php?id=<?php echo $quesrow['imageid']; ?>" />
-		<br/>
-	<?php
-		}
-	?>
-<?php		
-	}
-	echo "<br/>";
-    }
-?>
-<input type='submit' />
-</form>
-</body>
