b0b2528e399d8014b54a66c9455486eff0bb81bb
Ankit Gupta <ankigupt@gmail.com>
2016-08-15 21:04:51 +0530

    correction for the insert statement

--- model/AddCategory.php
+++ model/AddCategory.php
@@ -2,7 +2,7 @@
 		require_once('../model/db.php');
 		function addCategory($name){
 			$database_obj=connectDB();
-			$sql="insert into category values ('".mysql_real_escape_string($name)."');";
+			$sql="insert into category(CatName) values ('".mysql_real_escape_string($name)."');";
 			echo $sql;
 			$_SESSION['msg']=33;
 			$database_obj->query($sql);
