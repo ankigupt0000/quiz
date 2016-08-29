f8879b2459757f9f5f2014fd6964d355133c75e1
ankigupt0000 <ankigupt@gmail.com>
2016-08-29 17:41:32 +0530

    model changes
    model changes for edit quiz

--- model/CreateQuiz.php
+++ model/CreateQuiz.php
@@ -3,8 +3,32 @@
 		function CreateQuiz($name,$desc,$duration){
 			$database_obj=connectDB();
 			$sql="insert into quiz(QuizName,Description,duration) values ('".$name."','".$desc."',".$duration.");";
-			echo $sql;
-			$_SESSION['msg']=32;
+			//echo $sql;
+			$_SESSION['msg']=35;
 			$database_obj->query($sql);
 		}
+		function updateQuiz($qid,$name,$desc,$duration){
+			$database_obj=connectDB();
+			$sql="update quiz set Description='".$desc."', duration='".$duration."', QuizName='".$name."' where QuizId='".$qid."'";
+			$database_obj->query($sql);
+			$_SESSION['msg']=34;
+			echo $sql;
+			$_SESSION['qid']=$qid;
+			$_SESSION['qname']=$name;
+			$_SESSION['desc']=$desc;
+			$_SESSION['duration']=$duration;
+			
+		}
+		function getQuizInfo($id)
+		{
+			$database_obj=connectDB();
+			$sql="select QuizId,QuizName,Duration,Description from quiz where QuizId=".$id;
+			foreach($database_obj->query($sql) as $row )
+			{
+				$_SESSION['qid']=$row['QuizId'];
+				$_SESSION['qname']=$row['QuizName'];
+				$_SESSION['desc']=$row['Description'];
+				$_SESSION['duration']=$row['Duration'];
+			}
+		}	
 ?>
