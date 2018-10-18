bde5fbe29918989aacdfbb67849fb3141db8d0e1
ankigupt0000 <ankigupt@gmail.com>
2018-10-18 06:54:13 +0530

    Update db.php

--- model/db.php
+++ model/db.php
@@ -1,7 +1,7 @@
 <?php
 	function connectDB(){
-		$db_dsn="mysql:dbname=u898788349_qms;host=localhost;port=3306;mysql_connect_timeout=60";
-		$db_user="u898788349_qms";
+		$db_dsn="mysql:dbname=id7481567_u898788349_qms;host=localhost;port=3306;mysql_connect_timeout=60";
+		$db_user="id7481567_u898788349_qms";
 		$db_passwd="NOKIAN72";
 		return new PDO($db_dsn,$db_user,$db_passwd);
 	}
